37e60256ae6c6609f22c978c8270891bee7d990a
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-12-26 11:38:17 +0500

    WIP

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,5 +1,53 @@
 <template>
-  <div class="admin--post-page">
-
+  <div class="admin-new-post-page">
+    <section class="new-post-form">
+      <form action="http://echo.htmlacademy.ru" method="post" @submit.prevent="onSave">
+        <AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
+        <AppControlInput v-model="editedPost.title">Title</AppControlInput>
+        <AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>
+        <AppControlInput
+          control-type="textarea"
+          v-model="editedPost.content">
+          Content
+        </AppControlInput>
+        <AppButton type="submit">Save</AppButton>
+        <AppButton
+          type="button"
+          style="margin-left: 10px"
+          btn-style="cancel"
+          @click="onCancel">
+          Cancel
+        </AppButton>
+      </form>
+    </section>
   </div>
 </template>
+
+<script>
+import AppControlInput from '@/components/UI/AppControlInput.vue'
+import AppButton from '@/components/UI/AppButton.vue'
+export default {
+  data() {
+    return {
+      editedPost: {
+        author: '',
+        title: '',
+        thumbnailLink: '',
+        content: '',
+      }
+    }
+  },
+  methods: {
+    onSave() {
+      // Save the post
+    },
+    onCancel() {
+      // Navigate back
+    }
+  },
+  components: {
+    AppControlInput,
+    AppButton
+  }
+}
+</script>
