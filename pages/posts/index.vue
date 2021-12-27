bff547af1d81b48a2caf954b2d48100aa474d2fc
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-12-27 20:02:50 +0500

    WIP

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -11,53 +11,6 @@ export default {
   components: {
     appPostList: PostList,
   },
-  fetch(context) {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          loadedPosts: [
-            {
-              id: '1',
-              title: 'First Post',
-              previewText: 'This is our first post!',
-              thumbnail: 'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/129668669/original/aac8282bcfe964e60b0753c8c8b04efe23a42207/design-you-static-website-in-vuejs-ans-nuxtjs.png',
-            },
-            {
-              id: '2',
-              title: 'Second Post',
-              previewText: 'This is our second post!',
-              thumbnail: 'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/129668669/original/aac8282bcfe964e60b0753c8c8b04efe23a42207/design-you-static-website-in-vuejs-ans-nuxtjs.png',
-            },
-            {
-              id: '3',
-              title: 'Third Post',
-              previewText: 'This is our third post!',
-              thumbnail: 'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/129668669/original/aac8282bcfe964e60b0753c8c8b04efe23a42207/design-you-static-website-in-vuejs-ans-nuxtjs.png',
-            },
-            {
-              id: '4',
-              title: 'Fourth Post',
-              previewText: 'This is our fourth post!',
-              thumbnail: 'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/129668669/original/aac8282bcfe964e60b0753c8c8b04efe23a42207/design-you-static-website-in-vuejs-ans-nuxtjs.png',
-            },
-            {
-              id: '5',
-              title: 'Fifth Post',
-              previewText: 'This is our fifth post!',
-              thumbnail: 'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/129668669/original/aac8282bcfe964e60b0753c8c8b04efe23a42207/design-you-static-website-in-vuejs-ans-nuxtjs.png',
-            }
-          ]
-      });
-      }, 1000);
-      // reject(new Error());
-    })
-    .then(data => {
-      context.store.commit('setPost', data.loadedPosts);
-    })
-    .catch(e => {
-      context.error(new Error());
-    });
-  },
   computed: {
     loadedPosts() {
       return this.$store.getters.loadedPosts;
--- store/index.js
+++ store/index.js
@@ -11,6 +11,46 @@ const createStore = () => {
       }
     },
     actions: {
+      nuxtServerInit(vuexContext, context) {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            vuexContext.commit('setPost', [
+                {
+                  id: '1',
+                  title: 'First Post',
+                  previewText: 'This is our first post!',
+                  thumbnail: 'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/129668669/original/aac8282bcfe964e60b0753c8c8b04efe23a42207/design-you-static-website-in-vuejs-ans-nuxtjs.png',
+                },
+                {
+                  id: '2',
+                  title: 'Second Post',
+                  previewText: 'This is our second post!',
+                  thumbnail: 'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/129668669/original/aac8282bcfe964e60b0753c8c8b04efe23a42207/design-you-static-website-in-vuejs-ans-nuxtjs.png',
+                },
+                {
+                  id: '3',
+                  title: 'Third Post',
+                  previewText: 'This is our third post!',
+                  thumbnail: 'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/129668669/original/aac8282bcfe964e60b0753c8c8b04efe23a42207/design-you-static-website-in-vuejs-ans-nuxtjs.png',
+                },
+                {
+                  id: '4',
+                  title: 'Fourth Post',
+                  previewText: 'This is our fourth post!',
+                  thumbnail: 'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/129668669/original/aac8282bcfe964e60b0753c8c8b04efe23a42207/design-you-static-website-in-vuejs-ans-nuxtjs.png',
+                },
+                {
+                  id: '5',
+                  title: 'Fifth Post',
+                  previewText: 'This is our fifth post!',
+                  thumbnail: 'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/129668669/original/aac8282bcfe964e60b0753c8c8b04efe23a42207/design-you-static-website-in-vuejs-ans-nuxtjs.png',
+                }
+              ]);
+            resolve();
+          }, 1000);
+          // reject(new Error());
+        });
+      },
       setPosts(vuexContext, posts) {
         vuexContext.commit('setPost', posts)
       }
