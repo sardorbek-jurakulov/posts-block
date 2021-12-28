c7158149f6ed729574940a6a3b97d0d49824f39d
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2021-12-28 12:23:46 +0500

    WIP

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="admin-new-post-page">
     <section class="new-post-form">
-      <AdminPostForm />
+      <AdminPostForm @submit="onSubmitted"/>
     </section>
   </div>
 </template>
@@ -12,6 +12,11 @@ export default {
   layout: 'admin',
   components: {
     AdminPostForm,
+  },
+  methods: {
+    onSubmitted(postData) {
+
+    }
   }
 }
 </script>
