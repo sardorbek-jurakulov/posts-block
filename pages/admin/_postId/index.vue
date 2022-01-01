0e442f15b4698f87be55376d0889e15eb2db109b
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-01-01 14:47:25 +0500

    WIp

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -12,16 +12,13 @@ import axios from 'axios';
 export default {
   layout: 'admin',
   asyncData(context) {
-    return get('https://nuxt-blog-bdbbe-default-rtdb.asia-southeast1.firebasedatabase.app/posts/' + context.params.id + '.json')
-      .then()
-      .catch(e => context.error(e)) {
-      loadedPost: {
-        author: 'Sardorbek',
-        title: 'My awesome Post',
-        content: 'Super amazing, thanks for that!',
-        thumbnailLink: 'https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/129668669/original/aac8282bcfe964e60b0753c8c8b04efe23a42207/design-you-static-website-in-vuejs-ans-nuxtjs.png',
-      }
-    }
+    axios.get('https://nuxt-blog-bdbbe-default-rtdb.asia-southeast1.firebasedatabase.app/posts/' + context.params.postId + '.json')
+      .then(res => {
+        return {
+          loadedPost: res.data,
+        }
+      })
+      .catch(e => context.error());
   },
   components: {
     AdminPostForm,
