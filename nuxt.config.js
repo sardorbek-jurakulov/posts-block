c33f2cb05aacd82f5fcdc1fc89a00c5437f2e72b
sardorbek-jurakulov <sardorbek.juraqulov@gmail.com>
2022-01-05 18:12:45 +0500

    WIp

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,11 +27,12 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '~/assets/styles/main.css'
+    '~assets/styles/main.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~plugins/core-components.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
